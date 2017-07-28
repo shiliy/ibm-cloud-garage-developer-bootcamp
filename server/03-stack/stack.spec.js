7be40565eaff74eec127957972d4952304880de1
Shili Yang <shiliy@ca.ibm.com>
2017-07-28 17:51:29 -0400

    re-implement stack

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,3 +1,48 @@
-describe('the stack spec', () => {
+// let stack = {
+//  isEmpty: () => true,
+//  size: 0,
+//  push: () => {}
+//};
+const makeStack = () => {
+  let length = 0;
+  return {
+    isEmpty: () => length === 0,
+    push: () => length++,
+    size: () => length
+  };
+};
 
+let stack;
+
+describe.only('the stack spec', () => {
+
+  beforeEach(() => {
+    stack = makeStack();
+  });
+
+  it('starts empty', () => {
+    stack.isEmpty().should.be.true();
+  });
+
+  it('starts with stack size 0', () => {
+    stack.size().should.equal(0);
+  });
+
+  it('is not be empty when pushed', () => {
+    stack.push();
+    stack.isEmpty().should.false();
+  });
+
+  it('leaves stack size 1 when pushed', () => {
+    stack.push();
+    stack.size().should.equal(1);
+  });
+
+  it('leaves stack empty when pushed and popped');
+  it('leaves stack sie 0 when pushed and popped');
+  it('overflows');
+  it('under-flows');
+  it('pops the same one pushed');
+  it('pops the same two pushed');
+  it('accepts only positive capacity');
 });

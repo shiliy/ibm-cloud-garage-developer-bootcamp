2cb502cda2c68ece51577b0892f253f048f5e40d
Shili Yang <shiliy@ca.ibm.com>
2017-07-28 16:48:19 -0400

    run palindrome only

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -1,4 +1,4 @@
-describe.only('the fahrenheit2celsius canary spec', () => {
+describe('the fahrenheit2celsius canary spec', () => {
   it('shows the infrastructure works', () => {
     true.should.be.true();
   });

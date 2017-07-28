cfd55774eba05381de41d72fa4f3d716158aa59a
Shili Yang <shiliy@ca.ibm.com>
2017-07-28 16:45:56 -0400

    complete palidrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,8 +3,10 @@ describe.only('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
-  function isPalindrome(word) {
-    return word.split('').reverse().join('') == word;
+  function isPalindrome(phrase) {
+
+    if (phrase.trim().length === 0) return false;
+    return phrase.split('').reverse().join('') === phrase;
   }
 
   describe('a palindrome', () => {
@@ -15,10 +17,22 @@ describe.only('the palindrome canary spec', () => {
     it('is not racecars', () => {
       isPalindrome('racecars').should.be.false();
     });
-    it('is not "race car"');
-    it('" " is not a palindrome');
-    it('"" is not a palindrome');
-    it('is "mom dad mom"');
+
+    it('is not "race car"', () => {
+      isPalindrome('race car').should.be.false();
+    });
+
+    it('" " is not a palindrome', () => {
+      isPalindrome('  ').should.be.false();
+    });
+
+    it('"" is not a palindrome', () => {
+      isPalindrome('').should.be.false();
+    });
+
+    it('is "mom dad mom"', () => {
+      isPalindrome('mom dad mom').should.be.true();
+    });
   });
 
 });

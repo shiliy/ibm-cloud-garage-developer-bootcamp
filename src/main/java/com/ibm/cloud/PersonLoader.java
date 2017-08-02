45c66029f5129a389ef35e17b1161825aa21ca04
Shili Yang <shiliy@ca.ibm.com>
2017-08-02 15:55:24 -0400

    complete refactoring Person and personLoader

--- src/main/java/com/ibm/cloud/PersonLoader.java
+++ src/main/java/com/ibm/cloud/PersonLoader.java
@@ -7,85 +7,30 @@ import java.util.Scanner;
 
 public class PersonLoader {
   public static List<Person> loadPersonRecordsFromSpaceFile() throws java.io.FileNotFoundException {
-    String inputFilePath = "./data/space.txt";
-    String[] elements = {Person.LAST_NAME, Person.FIRST_NAME, Person.IGNORE,
-    Person.GENDER, Person.DATE_OF_BIRTH, Person.COLOR};
-    String delimiter = "\\s+";
-    List<Person> personList = new ArrayList<Person>();
-
-    try(Scanner scanner = new Scanner(new File(inputFilePath))) {
-      while(scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        String[] tokens = line.split(" ");
-
-
-        try {
-          // Person person = new Person(line, elements, delimiter);
-          // personList.add(person);
-          personList.add(new Person(tokens[0], tokens[1], tokens[3], tokens[5],  tokens[4]));
-
-        }
-        catch(PersonParseException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-
-    return personList;
-    // return loadPersonRecords(inputFilePath, elements, delimiter);
+    return createPersonList("./data/space.txt", " ", new int[] {0,1,3,5,4});
   }
 
   public static List<Person> loadPersonRecordsFromPipeFile() throws java.io.FileNotFoundException {
-    String inputFilePath = "./data/pipe.txt";
-    String[] elements = {Person.LAST_NAME, Person.FIRST_NAME, Person.IGNORE,
-    Person.GENDER, Person.COLOR, Person.DATE_OF_BIRTH};
-    String delimiter = "\\s*\\|\\s*";
-    List<Person> personList = new ArrayList<Person>();
-
-    try(Scanner scanner = new Scanner(new File(inputFilePath))) {
-      while(scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        String[] tokens = line.split("\\|");
-        for (int i = 0; i < tokens.length; i++) {
-          tokens[i] = tokens[i].trim();
-        };
-
-        try {
-          // Person person = new Person(line, elements, delimiter);
-          // personList.add(person);
-          personList.add(new Person(tokens[0], tokens[1], tokens[3], tokens[4],  tokens[5]));
-
-        }
-        catch(PersonParseException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-
-    return personList;
-    // return loadPersonRecords(inputFilePath, elements, delimiter);
+    return createPersonList("./data/pipe.txt", "\\|" ,new int[] {0,1,3,4,5});
   }
 
   public static List<Person> loadPersonRecordsFromCommaFile() throws java.io.FileNotFoundException {
-    String inputFilePath = "./data/comma.txt";
-    String[] elements = {Person.LAST_NAME, Person.FIRST_NAME, Person.GENDER,
-    Person.COLOR, Person.DATE_OF_BIRTH};
-    String delimiter = "\\s*,\\s*";
+    return createPersonList("./data/comma.txt", ",", new int[]{0,1,2,3,4});
+  }
 
+  private static List<Person> createPersonList(String inputFilePath, String delimiter, int[] fields)  throws java.io.FileNotFoundException {
     List<Person> personList = new ArrayList<Person>();
 
     try(Scanner scanner = new Scanner(new File(inputFilePath))) {
       while(scanner.hasNextLine()) {
         String line = scanner.nextLine();
-        String[] tokens = line.split(",");
+        String[] tokens = line.split(delimiter);
         for (int i = 0; i < tokens.length; i++) {
           tokens[i] = tokens[i].trim();
         };
 
         try {
-          // Person person = new Person(line, elements, delimiter);
-          // personList.add(person);
-          personList.add(new Person(tokens[0], tokens[1], tokens[2], tokens[3],  tokens[4]));
+          personList.add(new Person(tokens[fields[0]], tokens[fields[1]], tokens[fields[2]], tokens[fields[3]],  tokens[fields[4]]));
 
         }
         catch(PersonParseException e) {
@@ -93,29 +38,6 @@ public class PersonLoader {
         }
       }
     }
-
-    return personList;
-    // return loadPersonRecords(inputFilePath, elements, delimiter);
-  }
-
-
-  private static List<Person> loadPersonRecords(String inputFilePath, String[] elements, String delimiter) throws java.io.FileNotFoundException {
-    List<Person> personList = new ArrayList<Person>();
-
-    try(Scanner scanner = new Scanner(new File(inputFilePath))) {
-      while(scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-
-        try {
-          Person person = new Person(line, elements, delimiter);
-          personList.add(person);
-        }
-        catch(PersonParseException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-
     return personList;
   }
 }
